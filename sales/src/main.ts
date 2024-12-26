f9beac4994cc816f08eb922263e097dbc014daaa
李垚 <244885878@qq.com>
2024-12-26 18:14:14 +0800

    feat: 子应用umd模式删除、后续修改为keep-alive模式

--- sales/src/main.ts
+++ sales/src/main.ts
@@ -1,47 +1,20 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { routes } from "./router";
-import routers from "./router";
+import router from "./router";
 import ElementPlus from "element-plus";
 import "./main.scss";
 import "element-plus/dist/index.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import "./permission";
-import { createRouter, createWebHistory } from "vue-router";
 
-// 👇 将渲染操作放入 mount 函数，子应用初始化时会自动执行
-let history: any = null;
-let router = null;
-let app: any = null;
+const app = createApp(App);
 
-window.mount = () => {
-  history = createWebHistory();
-  router = createRouter({
-    history,
-    routes,
-  });
-
-  app = createApp(App);
-  app.use(routers);
-  app.use(ElementPlus, { locale: zhCn });
-  app.mount("#app");
-  // 注册所有element-plus/icon
-  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component);
-  }
-};
-
-// 👇 将卸载操作放入 unmount 函数，就是上面步骤2中的卸载函数
-window.unmount = () => {
-  app.unmount();
-  history.destroy();
-  app = null;
-  router = null;
-  history = null;
-};
-
-// 如果不在微前端环境，则直接执行mount渲染
-if (!window.__MICRO_APP_ENVIRONMENT__) {
-  window.mount();
+// 注册所有element-plus/icon
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
 }
+
+app.use(ElementPlus, { locale: zhCn });
+app.use(router);
+app.mount("#app");
